86ff1dc382a1dc1952b979db1cdd4d15d6e01d13
Reshu gupta <124056347+Reshugupta2003@users.noreply.github.com>
2024-09-29 13:33:04 +0530

    First_Program

---
+++ FirstProgram.py
@@ -0,0 +1,37 @@
+# Print in Different line
+print("Hello World")
+print("This is a python")
+
+"""
+Print in same line
+
+name = "Reshu"
+age = 21
+print("My name is :", name, " and my age is :",age) 
+
+"""
+# Data Type
+x = str(3)
+y = float(3)
+print(type(x),",", type(y))
+
+a = b = c = 434674585687856
+print(a)
+
+x = 3 + 5j
+print(type(x), ",",x)
+
+# quote inside quote
+print("It's alright","," 'He is called "johny"')
+
+# boolean 
+print(bool("abc"), bool(None), bool(0), bool({2,45,6}))
+
+# Expression execution
+A, B = "2", 3
+txt = "@"
+print((A+txt)*B)
+y,x = 10,5.0
+c = x/y
+print(type(c), c)
+print(c, x//y)   #integer division vs simple division  
