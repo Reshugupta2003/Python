8db3898931145749c662652c9397bb1e2c5ff6ba
Reshu gupta <124056347+Reshugupta2003@users.noreply.github.com>
2024-09-29 13:33:35 +0530

    Conditional_Statement

---
+++ conditionalStatement.py
@@ -0,0 +1,27 @@
+# light = input("Enter the colour of light :")
+
+# if(light == "Red"):
+#     print("Please Stop!")
+# elif(light == "Green"):
+#     print("Goo..")
+# elif(light == "Yellow"):
+#     print("Ready to Go")
+# else:
+#     print("Light is broken")
+
+# Arthemetic Operator
+num1, num2 = 7,2
+
+sum = num1+num2
+diff = num1 - num2
+mul = num1*num2
+div = num1/num2
+
+print("add:", sum, "difference:", diff, "multipily:",mul, "dividion:",div)
+print("reminder:", num1%num2)
+print("b is power of a:",num1**num2)
+
+# Question
+a = int(input("Enter the size of edge of square :"))
+area = a**2
+print("Area of square :", area)
